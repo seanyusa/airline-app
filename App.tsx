08a8e5eecb4fd581b002000ca524cddcd7e5b686
seanyusa <seanyusa@gmail.com>
2022-09-04 22:51:24 -0500

    Revert "attempt to use react-native-reanimated-carousel"
    This reverts commit 091e7d0eee578a32c3b9beab33bd817b47197979.

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import "react-native-gesture-handler";
 import { StyleSheet, SafeAreaView } from "react-native";
 import HomeScreen from "./screens/HomeScreen";
 
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,13 +1,7 @@
-import {
-  useWindowDimensions,
-  ScrollView,
-  StyleSheet,
-  View,
-} from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import MemberBox from "../components/MemberBox";
 import TripsBox from "../components/TripsBox";
 import InfoHero from "../components/InfoHero";
-import Carousel from "react-native-reanimated-carousel";
 
 const styles = StyleSheet.create({
   container: {
@@ -19,42 +13,17 @@ const styles = StyleSheet.create({
   },
   infoArea: {
     marginTop: 12,
-    marginHorizontal: -16,
-    overflow: "visible",
   },
 });
 
 export default function HomeScreen() {
-  const { width } = useWindowDimensions();
-
   return (
     <View style={styles.container}>
       <ScrollView>
         <MemberBox />
         <TripsBox style={styles.trips} />
         <View style={styles.infoArea}>
-          <Carousel
-            loop={false}
-            width={width}
-            height={400}
-            data={[0, 0, 0, 0]}
-            scrollAnimationDuration={600}
-            mode="horizontal-stack"
-            // mode="parallax"
-            modeConfig={{
-              parallaxScrollingScale: 1,
-              parallaxScrollingOffset: 20,
-              stackInterval: 400,
-              scaleInterval: 0,
-              opacityInterval: 0,
-              rotateZDeg: 0,
-            }}
-            // onSnapToItem={(index) => console.log('current index:', index)}
-            renderItem={({ index }) => <InfoHero />}
-            panGestureHandlerProps={{
-              activeOffsetX: [-20, 20],
-            }}
-          />
+          <InfoHero />
         </View>
       </ScrollView>
     </View>
