510707e0f943b8ac1bbf174ebe6276f8b59e7ddd
seanyusa <seanyusa@gmail.com>
2022-09-05 13:27:11 -0500

    member box touchable

--- components/MemberBox.tsx
+++ components/MemberBox.tsx
@@ -1,13 +1,25 @@
-import { StyleSheet, Text, View, ViewStyle } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TouchableNativeFeedback,
+  View,
+  ViewStyle,
+} from "react-native";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#0b0b0baa",
-    alignItems: "flex-start",
     paddingHorizontal: 12,
     paddingTop: 16,
     paddingBottom: 20,
     borderRadius: 4,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: "flex-start",
   },
   name: {
     fontSize: 19,
@@ -26,10 +38,15 @@ type Props = {
 
 export default function MemberBox(props: Props) {
   return (
-    <View style={[styles.container, props.style]}>
-      <Text style={[styles.text, styles.name]}>Sean Wang</Text>
-      <Text style={styles.text}>AAdvantage member</Text>
-      <Text style={styles.text}>Available award miles: 32,030</Text>
-    </View>
+    <TouchableNativeFeedback onPress={() => {}}>
+      <View style={[styles.container, props.style]}>
+        <View style={styles.contentContainer}>
+          <Text style={[styles.text, styles.name]}>Sean Wang</Text>
+          <Text style={styles.text}>AAdvantage member</Text>
+          <Text style={styles.text}>Available award miles: 32,030</Text>
+        </View>
+        <MaterialIcons name="keyboard-arrow-right" size={24} color="#ffffff" />
+      </View>
+    </TouchableNativeFeedback>
   );
 }
