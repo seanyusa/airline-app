46b1c6a1db777e642ff92c647b0059f743b9bc25
seanyusa <seanyusa@gmail.com>
2022-09-05 09:26:04 -0500

    reorganize TripsBox

--- components/TripsBox.tsx
+++ components/TripsBox.tsx
@@ -4,28 +4,31 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#333333",
     alignItems: "flex-start",
-    paddingHorizontal: 12,
-    paddingVertical: 20,
     borderRadius: 4,
+  },
+  numTripsContainer: {
+    paddingHorizontal: 12,
+    paddingVertical: 16,
   },
   label: {
     fontSize: 17,
     color: "#bbbbbb",
-    marginBottom: 8,
+    marginBottom: 4,
     fontFamily: "OpenSans-Regular",
   },
   numTrips: {
     fontSize: 30,
     color: "#ffffff",
-    marginBottom: 12,
     fontFamily: "OpenSans-Light",
   },
   divider: {
     height: 1,
-    marginHorizontal: -12,
-    alignSelf: "stretch",
+    width: "100%",
     backgroundColor: "#555555",
-    marginBottom: 8,
+  },
+  tripRow: {
+    paddingHorizontal: 12,
+    paddingVertical: 12,
   },
   tripName: {
     fontSize: 22,
@@ -46,13 +49,17 @@ type Props = {
 export default function TripsBox(props: Props) {
   return (
     <View style={[styles.container, props.style]}>
-      <Text style={styles.label}>You have</Text>
-      <Text style={styles.numTrips}>1 upcoming trip</Text>
+      <View style={styles.numTripsContainer}>
+        <Text style={styles.label}>You have</Text>
+        <Text style={styles.numTrips}>1 upcoming trip</Text>
+      </View>
       <View style={styles.divider} />
-      <Text style={styles.tripName}>AUS -&gt; ORD</Text>
-      <Text style={styles.tripDetails}>
-        THURSDAY, SEPTEMBER 22, 2022 • XDDLSD
-      </Text>
+      <View style={styles.tripRow}>
+        <Text style={styles.tripName}>AUS -&gt; ORD</Text>
+        <Text style={styles.tripDetails}>
+          THURSDAY, SEPTEMBER 22, 2022 • XDDLSD
+        </Text>
+      </View>
     </View>
   );
 }
